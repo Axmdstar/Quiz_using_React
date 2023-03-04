d37fe60dd1d1e80449f876853c280b9722b68d1b
Axmdstar <85846658+Axmdstar@users.noreply.github.com>
2023-03-04 18:10:10 +0300

    fix bugs , added options

--- src/App.js
+++ src/App.js
@@ -11,17 +11,33 @@ function App() {
   const [title, settitle] = useState("Quiz");
   const [reset, setreset] = useState(false);
 
+
+  function selectinputs(){
+    setstartbtn(!startbtn);
+    setstarttext("Loading") 
+  };
+
   function ResetBtn() {
     setstartbtn(false);
     setQuesJson(null);
     setreset(false);
     setstarttext("START");
-  }
+  };
 
   //! Effect Function 
   useEffect(() => {
+    let dblink = 'https://opentdb.com/api.php?amount=10'
+
     if (startbtn) {
-      fetch('https://opentdb.com/api.php?amount=10')
+
+      const catagory = document.getElementById("ctgy");
+      const diffculty = document.getElementById("diff");
+      
+      dblink += `&category=${catagory.value}`;
+      dblink += `&difficulty=${diffculty.value}`;
+
+
+      fetch(dblink)
         .then((result) => {
           settitle("Quiz")
           return result.json();
@@ -34,13 +50,14 @@ function App() {
           settitle("Error")
         })
     }
-  }, [startbtn])
+  }, [ startbtn])
 
   return (
     <div className="mainQuiz">
-      <div className="header">
 
+      <div className="header">
         <h1>{title}</h1>
+
         <div className="icons">
 
           <a href="https://www.youtube.com/channel/UCDkUQx2-sfisF8xiiCW_ILw">
@@ -59,17 +76,56 @@ function App() {
       </div>
       {QuesJson === null
         ?
-        <button className="start" onClick={() => { setstartbtn(!startbtn); setstarttext("Loading...") }} > {starttext}</button>
+        <button className="start" type="button" onClick={selectinputs} > {starttext} </button>
         :
         <div >
           {QuesJson && <QuizBrain Ques={QuesJson} ResetBtn={ResetBtn} />}
         </div>
       }
       {!QuesJson &&  
-      <p style={{color:"white", lineHeight:"30px"}}>Introducing my latest project, a simple and fun quiz game! With just a click of a button, my project retrieves 10 random
-         questions from a trusted source and presents them to the user. The user can choose to skip questions and come back to them
-        later. After answering all the questions, the project displays the final result. It's a great way to test your knowledge
-          and have fun at the same time!</p>
+      <div >
+
+      <div className="OptionBox">
+
+        <div className="category">
+          <p>Category:</p>
+
+          <div className="select">
+            <select name="option" id="ctgy">
+                <option value="9">General Knowledge</option>
+                <option value="18">Science: Computers</option>
+                <option value="17">Science & Nature</option>
+                <option value="19">Science: Mathematics</option>
+                <option value="20">Mythology</option>
+                <option value="21">Sports</option>
+                <option value="22">Geography</option>
+                <option value="23">History</option>
+                <option value="24">Politics</option>
+                <option value="25">Art</option>
+                <option value="27">Animals</option>
+                <option value="30">Science: Gadgets</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="diffculty">
+          <p>Diffculty:</p>
+
+          <div className="select">
+            <select id="diff">
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+            </select>
+          </div>
+        </div>
+
+      </div>
+        <p style={{color:"white", lineHeight:"30px"}}>Introducing my latest project, a simple and fun quiz game! With just a click of a button, my project retrieves 10 random
+           questions from a trusted source and presents them to the user. The user can choose to skip questions and come back to them
+          later. After answering all the questions, the project displays the final result. It's a great way to test your knowledge
+            and have fun at the same time!</p>
+      </div>
           }
     </div>
   );
